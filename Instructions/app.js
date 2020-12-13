4bc0763d582440b12699123bef03677ff1e026d8
zach-18 <67443071+zach-18@users.noreply.github.com>
2020-12-13 12:24:39 -0600

    read sample data

--- Instructions/app.js
+++ Instructions/app.js
@@ -3,7 +3,7 @@
 
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("samples.json").then((importedData) => {
+d3.json("data/samples.json").then((importedData) => {
     console.log(importedData);
     var data = importedData;
 });
